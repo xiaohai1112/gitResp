529f8021e44805e509e1ceef7ccf2576f55241cf
wzs <2547761599@qq.com>
2025-10-09 21:22:50 +0800

    插入用户

--- service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
@@ -1,12 +1,15 @@
 package com.msb.servicepassengeruser.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
 import com.msb.dao.ResponseResult;
 import com.msb.servicepassengeruser.mapper.UserMapper;
 import com.msb.servicepassengeruser.pojo.PassengerUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +26,21 @@ public class UserService {
         List<PassengerUser> passengerUserList = userMapper.selectByMap(map);
         System.out.println(passengerUserList.size()==0?"无记录":passengerUserList.get(0).getPassengerName());
         //根据手机号插入用户
+        if (passengerUserList.size()==0){
+            PassengerUser puser = new PassengerUser();
+            puser.setPassengerName("菲菲");
+            puser.setPassengerPhone(passengerPhone);
+            puser.setPassengerGender((byte) 0);
+            puser.setState((byte) 0);
+
+            LocalDateTime now=LocalDateTime.now();//比java.util.date线程更安全
+            // LocalDateTime → Timestamp → Date
+            Timestamp timestamp = Timestamp.valueOf(now);
+            puser.setGmtCreate(timestamp);
+            puser.setGmtModified(timestamp);
+
+            userMapper.insert(puser);
+        }
         //返回成功
         return ResponseResult.success();
     }
